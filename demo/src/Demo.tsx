1f0def2ed810377d698386951951bb957af0837c
alexkatz <alexanderikatz@gmail.com>
2017-08-11 22:40:04 -0400

    initial commit

---
+++ demo/src/Demo.tsx
@@ -0,0 +1,3 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import * as Popover from 'react-popover-typescript';
---
+++ src/Popover.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+
+export interface PopoverState {
+
+}
+
+export interface PopoverProps {
+
+}
+
+class Popover extends React.Component<PopoverProps, PopoverState> {
+
+}
+
+// tslint:disable-next-line:no-default-export
+export default Popover;
