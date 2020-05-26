5d778efdcb1985b6786e248592b2b68e03d969e5
Alex Katz <alexanderikatz@gmail.com>
2020-05-25 23:24:44 -0400

    Missing file

---
+++ docs/src/constants.ts
@@ -0,0 +1,13 @@
+export const NO_SELECT: React.CSSProperties = {
+  userSelect: 'none',
+  MozUserSelect: 'none',
+  WebkitUserSelect: 'none',
+  WebkitTouchCallout: 'none',
+};
+
+export const FONT: React.CSSProperties = {
+  color: 'white',
+  fontFamily: 'sans-serif',
+  fontWeight: 100,
+  fontSize: 12,
+};
