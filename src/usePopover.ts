alexkatz/react-tiny-popover

commit d4eac0a767594732308699c357185053263c1cd7

added rounding to both transform locations

src/usePopover.ts
@@ -68,12 +68,10 @@ export const usePopover = ({
               })
             : contentLocation;
 
-        const left = parentRect.left + inputLeft;
-        const top = parentRect.top + inputTop;
+        const left = Math.round(parentRect.left + inputLeft - scoutRect.left);
+        const top = Math.round(parentRect.top + inputTop - scoutRect.top);
 
-        popoverRef.current.style.transform = `translate(${Math.round(left - scoutRect.left)}px, ${
-          Math.round(top - scoutRect.top)
-        }px)`;
+        popoverRef.current.style.transform = `translate(${left}px, ${top}px)`;
 
         onPositionPopover({
           childRect,
@@ -134,9 +132,10 @@ export const usePopover = ({
         finalLeft = nudgedLeft;
       }
 
-      popoverRef.current.style.transform = `translate(${finalLeft - scoutRect.left}px, ${
-        finalTop - scoutRect.top
-      }px)`;
+      finalTop = Math.round(finalTop - scoutRect.top);
+      finalLeft = Math.round(finalLeft - scoutRect.left);
+
+      popoverRef.current.style.transform = `translate(${left}px, ${top}px)`;
 
       const potentialViolations: BoundaryViolations = {
         top: boundaryRect.top + boundaryInset - finalTop,

commit 0efa2ad81bfa88ad79620fb9d98241e886f9b7a7

Fix blur on non-retina displays + popover overflow
This commit fixes a regression introduced in d4eac0a767594732308699c357185053263c1cd7 which allowed the popover to appear outside the viewport. The actual transform style applied did not take the scoutRect into account anymore.

Additionally, this fix also addresses the blurry dropdown issue on non-retina displays as initially intended by #176.

src/usePopover.ts
@@ -135,7 +135,7 @@ export const usePopover = ({
       finalTop = Math.round(finalTop - scoutRect.top);
       finalLeft = Math.round(finalLeft - scoutRect.left);
 
-      popoverRef.current.style.transform = `translate(${left}px, ${top}px)`;
+      popoverRef.current.style.transform = `translate(${finalLeft}px, ${finalTop}px)`;
 
       const potentialViolations: BoundaryViolations = {
         top: boundaryRect.top + boundaryInset - finalTop,